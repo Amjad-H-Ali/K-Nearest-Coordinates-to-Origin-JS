860f50d0b1eed5aa8015724015dbc72f99235506
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2019-01-21 21:03:15 -0600

    Created function that generates an array of coordinates. Configured class a bit

--- JS/index.js
+++ JS/index.js
@@ -2,10 +2,24 @@ console.log("K Nearest Coordinates to Origin");
 
 // To instantiate a coordinate object
 class Coordinate {
-	constructor(x, y) {
-		this.x = x;
-
-		this.y = y;
+	constructor() {
+		// Generates random values -10-10
+		this.x = Math.floor(Math.random() * 21) - 10 ;
+		// Generates random values -10-10
+		this.y = Math.floor(Math.random() * 21) - 10;
 	}
 }
 
+// Generates array of given size with coordinate objects
+const randomCoordinatesArray = (size) => {
+
+	const array =[];
+
+	for(let i = 0; i < size; i ++ )
+
+		array.push(new Coordinate());
+
+	return array;
+}
+
+console.log(randomCoordinatesArray(5));
